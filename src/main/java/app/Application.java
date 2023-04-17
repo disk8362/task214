b84dfc806a8363a037f614806f86290ed48f8a0c
disk8362 <disk8362@mail.ru>
2023-04-18 01:31:31 +0300

    git commit

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,12 +1,9 @@
 package app;
-
 import app.config.AppConfig;
 import app.model.AnimalsCage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 public class Application {
-
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
@@ -16,5 +13,4 @@ public class Application {
             bean.whatAnimalSay();
         }
     }
-
 }
--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -1,7 +1,5 @@
 package app.model;
-
 import org.springframework.stereotype.Component;
-
 @Component
 public class Cat extends Animal {
     @Override
